c6ab15172ae167eb71f2bac5d3d4d722c514341b
Immanuel <aimmanuel925@gmail.com>
2025-05-27 00:33:41 +0100

    Chanage router history from webHistory to webHasHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import Home from "../views/Home.vue"
 import Login from "../views/auth/login.vue"
 import RegisterSeller from "../views/auth/ResgisterSeller.vue"
@@ -30,7 +30,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
